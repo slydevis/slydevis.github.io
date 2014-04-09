11230582906f2175df5cd0e9eeeebe39d0d31ba7
Ragnos <slydevis@hotmail.fr>
2014-04-09 12:57:00 +0200

    Update 9 avril 2014

---
+++ post.php
@@ -0,0 +1,101 @@
+<?php
+
+function verifMail($mail) {
+	if(preg_match("#^[a-z0-9._-]+@[a-z0-9._-]{2,}\.[a-z]{2,4}$#", $mail) && (!empty($mail)))
+		return true;
+	else 
+		return false;
+}
+
+function verifPhone($phone) {
+    if (preg_match("#^0[1-68]([-. ]?[0-9]{2}){4}$#", $phone) || empty($phone))
+    	return true;
+    else
+        return false;
+}
+
+$message = "";
+$nom = "";
+$prenom = "";
+$phone = "";
+$mail = "";
+$message = "";
+
+if (isset($_POST["nom"])) {
+	$nom = htmlentities($_POST["nom"]);
+}
+
+if (isset($_POST["prenom"])) {
+	$prenom = htmlentities($_POST["prenom"]);
+}
+
+if(isset($_POST["phone"])) {
+	$phone = htmlentities($_POST["phone"]);
+}
+
+if(isset($_POST['mail'])) {
+	$mail = htmlentities($_POST["mail"]);
+}
+
+if(isset($_POST['message'])) {
+	$message = htmlentities($_POST['message']);
+}
+
+$objet = new StdClass();
+if ((!empty($nom)) && (!empty($prenom)) && verifPhone($phone) && (!empty($message)) && verifMail($mail)) {
+	$objet->message
+		= "Votre message à bien été transmis.";
+	$objet->result = true;
+} else {
+	$objet->message = "Game Over votre ordinateur s'auto détruira dans 5sec";
+	$objet->result = false;
+}
+
+$mailAdmin = 'slydevis@hotmail.fr'; // Déclaration de l'adresse de destination.
+if (!preg_match("#^[a-z0-9._-]+@(hotmail|live|msn).[a-z]{2,4}$#", $mailAdmin)) // On filtre les serveurs qui rencontrent des bogues.
+{
+	$passage_ligne = "\r\n";
+}
+else
+{
+	$passage_ligne = "\n";
+}
+
+//=====Déclaration des messages au format texte et au format HTML.
+$message_html = "<html><head></head><body>Vous avez un message de ".$nom." ".$prenom." :<br/>".$messages."<br/>---------------------<br/><br/>Voici les coordonées de cet utilisateur :<br/>Téléphone :".$phone."<br/>Mail : ".$mail;
+//==========
+ 
+//=====Création de la boundary
+$boundary = "-----=".md5(rand());
+//==========
+ 
+//=====Définition du sujet.
+$sujet = "Message de ".$nom." ".$prenom;
+//=========
+ 
+//=====Création du header de l'e-mail.
+$header = "From: Message de \"".$nom." ".$prenom."\"<".$mail.">".$passage_ligne;
+$header.= "Reply-to: Message de \"".$nom." ".$prenom."\" <".$mail.">".$passage_ligne;
+$header.= "MIME-Version: 1.0".$passage_ligne;
+$header.= "Content-Type: multipart/alternative;".$passage_ligne." boundary=\"$boundary\"".$passage_ligne;
+//==========
+ 
+//=====Création du message.
+$message = $passage_ligne."--".$boundary.$passage_ligne;
+//=====Ajout du message au format HTML
+$message.= "Content-Type: text/html; charset=\"utf-8\"".$passage_ligne;
+$message.= "Content-Transfer-Encoding: 8bit".$passage_ligne;
+$message.= $passage_ligne.$message_html.$passage_ligne;
+//==========
+$message.= $passage_ligne."--".$boundary."--".$passage_ligne;
+//==========
+ 
+//=====Envoi de l'e-mail.
+mail($mailAdmin,$sujet,$message,$header);
+//==========
+
+header('Cache-Control: no-cache, must-revalidate');
+header('Expires: Mon, 26 Jul 1997 05:00:00 GMT');
+header('Content-type: application/json');
+
+echo json_encode($objet);
--- script.js
+++ script.js
@@ -1,32 +1,193 @@
+// TODO : Mettre le contenu du slide service
+// TODO : Mettre le contenu du slide Portofolio (carousel)
+
 $(document).ready(function() {
-	$("#test").hide();
+
+	$("#portofolio").hide();
+	$('#service').hide();
 	$(".title").hide();
+	$('.alert.alert-success').hide();
+	$('.alert.alert-danger').hide();
+
+	$("button:contains('Envoyer')").attr("disabled","disabled").addClass( 'ui-state-disabled' ); // Désactive Le bouton envoyer
+
 	$(".cameleon1").stop().animate({opacity: 0},2000,function(){
 		$(".title").stop().fadeIn(1);
-		$(this).css({'background-image': "url('img/cameleon1.png')"}).animate({opacity: 1},{duration:2000});});
-	
- 	var sens = true;
- 	$("#test2").click(function(){
- 		$("#test").show();
- 		if(sens)
- 		{
- 			console.log(sens);
- 			sens = false;
-			$("#test").animate({"left": "-=1550px"}, "slow");
- 		}
-		else
-		{
-			console.log(sens);
-			sens = true;
-			$("#test").animate({"left": "+=1550px"}, "slow");
-			$("#test").hide(1);
-		}
+		$(this).css({'background-image': "url('img/cameleon1.png')"}).animate({opacity: 1},{duration:2000});
 	});
-     $('a[href=#top]').click(function(){
-          $('html, body').animate({scrollTop:0}, 'slow');
-          return false;
-     });
 
+	function validerFormulaire() {
+
+		var champsNom = $('.Nom .glyphicon').hasClass('glyphicon-ok');
+		var champsPrenom = $('.Prenom .glyphicon').hasClass('glyphicon-ok');
+		var champsPhone = $('.Phone .glyphicon').hasClass('glyphicon-remove');
+		var champsMail = $('.Mail .glyphicon').hasClass('glyphicon-ok');
+		var champsMess = $('.Message .glyphicon').hasClass('glyphicon-ok');
+
+		if(champsNom && champsPrenom && (!champsPhone) && champsMail && champsMess)
+			$("button:contains('Envoyer')").removeAttr("disabled").removeClass( 'ui-state-disabled' );
+		else {
+			if(!$("button:contains('Envoyer')").attr("disabled","disabled").hasClass('ui-state-disabled')) 
+				$("button:contains('Envoyer')").attr("disabled","disabled").addClass( 'ui-state-disabled' );
+		}
+	}
+
+	$("#Nom").blur(function() {
+		if($(this).val() == "") {
+			$(this).parent(".form-group").removeClass('has-success has-warning');
+			$(this).parent(".form-group").addClass('has-error has-feedback');
+			$(".Nom .glyphicon").removeClass('glyphicon-ok glyphicon-warning')
+	  		$(".Nom .glyphicon").addClass('glyphicon-remove');
+	  		$(".Nom .help-block").html("<span class=\"help-block\">Le champs ne dois pas être vide</span>");
+		}
+		else {
+			$(this).parent(".form-group").removeClass('has-error has-warning');
+			$(this).parent(".form-group").addClass('has-success has-feedback');
+			$(".Nom .glyphicon").removeClass('glyphicon-remove glyphicon-warning')
+			$(".Nom .glyphicon").addClass("glyphicon-ok");
+			$(".Nom .help-block").html("<span class=\"help-block\">Saisie correcte</span>");
+		}
+  		validerFormulaire();
+	});
+
+	$("#Prenom").blur(function() {
+		if($(this).val() == "") {
+			$(this).parent(".form-group").removeClass('has-success has-warning');
+			$(this).parent(".form-group").addClass('has-error has-feedback');
+			$(".Prenom .glyphicon").removeClass('glyphicon-ok glyphicon-warning')
+	  		$(".Prenom .glyphicon").addClass('glyphicon-remove');
+	  		$(".Prenom .help-block").html("<span class=\"help-block\">Le champs ne dois pas être vide</span>");
+		}
+		else {
+			$(this).parent(".form-group").removeClass('has-error has-warning');
+			$(this).parent(".form-group").addClass('has-success has-feedback');
+			$(".Prenom .glyphicon").removeClass('glyphicon-remove glyphicon-warning')
+			$(".Prenom .glyphicon").addClass("glyphicon-ok");
+			$(".Prenom .help-block").html("<span class=\"help-block\">Saisie correcte</span>");
+		}
+  		validerFormulaire();
+	});
+
+	$("#Phone").blur(function() {
+		if((!$(this).val().match("^0[1-68]([-. ]?[0-9]{2}){4}$"))) {
+			$(this).parent(".form-group").removeClass('has-success has-warning');
+			$(this).parent(".form-group").addClass('has-error has-feedback');
+			$(".Phone .glyphicon").removeClass('glyphicon-ok glyphicon-warning')
+	  		$(".Phone .glyphicon").addClass('glyphicon-remove');
+	  		$(".Phone .help-block").html("<span class=\"help-block\">Le numéro de téléphone dois être valide</span>");
+		}
+		else {
+			$(this).parent(".form-group").removeClass('has-error has-warning');
+			$(this).parent(".form-group").addClass('has-success has-feedback');
+			$(".Phone .glyphicon").removeClass('glyphicon-remove glyphicon-warning')
+			$(".Phone .glyphicon").addClass("glyphicon-ok");
+			$(".Phone .help-block").html("<span class=\"help-block\">Saisie correcte</span>");
+		}
+  		validerFormulaire();
+	});
+
+	$("#Mail").blur(function() {
+		
+		if((!$(this).val().match("^[a-z0-9._-]+@[a-z0-9._-]{2,}\.[a-z]{2,4}$")) || $(this).val() == '') {
+			$(this).parent(".form-group").removeClass('has-success has-warning');
+			$(this).parent(".form-group").addClass('has-error has-feedback');
+			$(".Mail .glyphicon").removeClass('glyphicon-ok glyphicon-warning')
+	  		$(".Mail .glyphicon").addClass('glyphicon-remove');
+	  		$(".Mail .help-block").html("<span class=\"help-block\">L'adresse email dois être valide</span>");
+		}
+		else {
+			$(this).parent(".form-group").removeClass('has-error has-warning');
+			$(this).parent(".form-group").addClass('has-success has-feedback');
+			$(".Mail .glyphicon").removeClass('glyphicon-remove glyphicon-warning')
+			$(".Mail .glyphicon").addClass("glyphicon-ok");
+			$(".Mail .help-block").html("<span class=\"help-block\">Saisie correcte</span>");
+		}
+  		validerFormulaire();
+	});
+
+	$("#Message").blur(function() {
+		if($(this).val() == "") {
+			$(this).parent(".form-group").removeClass('has-success has-warning');
+			$(this).parent(".form-group").addClass('has-error has-feedback');
+			$(".Message .glyphicon").removeClass('glyphicon-ok glyphicon-warning')
+	  		$(".Message .glyphicon").addClass('glyphicon-remove');
+	  		$(".Message .help-block").html("<span class=\"help-block\">Le champs ne dois pas être vide</span>");
+		}
+		else {
+			$(this).parent(".form-group").removeClass('has-error has-warning');
+			$(this).parent(".form-group").addClass('has-success has-feedback');
+			$(".Message .glyphicon").removeClass('glyphicon-remove glyphicon-warning')
+			$(".Message .glyphicon").addClass("glyphicon-ok");
+			$(".Message .help-block").html("<span class=\"help-block\">Saisie correcte</span>");
+		}
+  		validerFormulaire();
+	});
+ 	
+ 	function slide(obj, sens) {
+ 		var object = $(obj);
+ 		var toPorto = $('#portofolio');
+ 		$(obj).show();
+ 		if(sens) {
+ 			$(obj).animate({"left" : "-=1550px"}, "slow");
+ 		}
+ 		else {
+ 			$(obj).animate({"left": "+=1550px"}, "slow");
+ 			$(obj).hide(1);
+ 		}
+ 	}
+
+	var sensPorto = true;
+ 	$('#portofolio-menu').click(function() {
+ 		if(!sensService) {
+ 			$('#service').hide();
+ 			slide('#service', sensService);
+ 			sensService = true;
+ 		}
+ 		slide('#portofolio', sensPorto);
+		sensPorto = (!sensPorto);
+ 		return false;
+ 	});
+
+ 	var sensService = true;
+ 	$('#service-menu').click(function() {
+ 		if(!sensPorto) {
+ 			$('#portofolio').hide();
+ 			slide('#portofolio', sensPorto);
+ 			sensPorto = true;
+ 		}
+ 		slide('#service', sensService);
+ 		sensService = (!sensService);
+ 		return false;
+ 	});
+
+ 	$('#acceuil, a[href=#contact]').click(function() {
+ 		$('.title').show();
+ 		if(!sensPorto) {
+ 			$('#portofolio').hide();
+ 			slide('#portofolio', sensPorto);
+ 			sensPorto = true;
+ 		}
+ 		if(!sensService) {
+ 			$('#service').hide();
+ 			slide('#service', sensService);
+ 			sensService = true;
+ 		}
+ 		return false;
+ 	});
+
+	$('ul.navbar-nav li a').click(function () {
+		if(!$(this).parent('li.active').hasClass("active")) {
+			$('ul.navbar-nav li.active').removeClass('active');
+			$(this).parent('li').addClass('active');
+		}
+		else {
+			$(this).parent('li.active').removeClass('active');
+			$('#acceuil').parent('li').addClass('active');
+		}
+		return false;
+	});
+
+ 	
  	$('.scrollTo').click( function() { // Au clic sur un élément
       var page = $(this).attr('href'); // Page cible
       var speed = 750; // Durée de l'animation (en ms)
@@ -34,4 +195,46 @@ $(document).ready(function() {
 
       return false;
     });
+
+    $('form').submit(function() {
+		$("#Nom").attr("disabled","disabled");
+		$("#Prenom").attr("disabled","disabled");
+		$("#Phone").attr("disabled","disabled");
+		$("#Mail").attr("disabled","disabled");
+		$("#Message").attr("disabled","disabled");
+		$.post($(this).attr('action'),$(this).serialize(),function(texte){
+			$(':button:contains(\'Envoyer\')').fadeOut(200);
+		});
+		
+		$.ajax({
+	        type: $(this).attr("method"),
+	        url: $(this).attr("action"),
+	        data: $(this).serialize(),
+	        success: OnSuccess,
+			error: OnError
+		});
+
+		function OnSuccess(result) {
+			var message = result.message;
+			var resultat = result.result;
+			$('form').fadeOut('slow');
+			if(resultat) {
+				$('.alert.alert-success').fadeIn('slow');
+				$('a[href=#contact]').fadeOut('slow');
+				$('.alert.alert-success').html(message);
+			}
+			else {
+				$('.alert.alert-danger').fadeIn('slow');
+				$('.alert.alert-danger').html(message);
+			}
+		}
+
+		function OnError(result) {
+			$('form').fadeOut('slow');
+			$('.alert.alert-danger').fadeIn('slow');
+			$('.alert.alert-danger').html("Une erreur c'est produite veuillez actualiser la page et recommencer");
+		}
+		
+		return false;
+	})
 });
